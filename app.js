b6db252278e7d82c9e33e1d7cad464d049317e91
Calvin Langer <calvin.langerr@gmail.com>
2021-12-02 18:34:17 +0100

    calculator without equals

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ result = document.getElementById("viewer");
 numbers = document.getElementsByClassName("num");
 clear = document.getElementById("clear");
 plus = document.getElementById("plus");
+minus = document.getElementById("minus");
+multiplication = document.getElementById("multiplication");
+division = document.getElementById("division");
+dot = document.getElementById("dot");
+equal = document.getElementById("equals");
+
+const operations = new Set();
+operations.add("+");
+operations.add("-");
+operations.add("*");
+operations.add("/");
+operations.add(".");
 
 const numberClickedListener = function () {
   const number = this.getAttribute("data-num");
@@ -20,22 +32,22 @@ const clearButtonListener = function () {
   result.innerHTML = 0;
 };
 
+const operationListener = function (operation) {
+  const lastChar = result.innerHTML.charAt(result.innerHTML.length - 1);
+
+  if (!operations.has(lastChar)) {
+    result.innerHTML = result.innerHTML + operation;
+  }
+};
+
+const equalButtonListener = function () {
+  console.log("you did it!");
+};
+
 clear.addEventListener("click", clearButtonListener);
-
-switch () {
-  case "plus":
-    result.innerHTML = number + number;
-    break;
-
-  case "minus":
-    result.innerHTML = number - number;
-    break;
-
-  case "times":
-    result.innerHTML = number * number;
-    break;
-
-  case "divided by":
-    result.innerHTML = number / number;
-    break;
-}
+plus.addEventListener("click", () => operationListener("+"));
+minus.addEventListener("click", () => operationListener("-"));
+division.addEventListener("click", () => operationListener("/"));
+multiplication.addEventListener("click", () => operationListener("*"));
+dot.addEventListener("click", () => operationListener("."));
+equal.addEventListener("click", equalButtonListener);
